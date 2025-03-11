b667f6a6287a481c619b7a421514f07e30af8e44
Wolvye <alainsoltau@googlemail.com>
2025-03-11 15:58:45 +0100

    Updated PlayerMovement script and added new input actions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xClampRange = 5f;
+    [SerializeField] float yClampRange = 5f;
 
-    // Update is called once per frame
+    [SerializeField] float controlRollFactor = 20f;
+    [SerializeField] float controlPitchFactor = 18f;
+    [SerializeField] float rotationSpeed = 10f;
+
+    Vector2 movement;
+
     void Update()
     {
-        
+        ProcessTranslation();
+        ProcessRotation();
     }
+
+    public void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+    }
+    void ProcessRotation()
+    {
+        float pitch = -controlPitchFactor * movement.y;
+        float roll = -controlRollFactor * movement.x;
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+
 }
